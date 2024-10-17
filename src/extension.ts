bf76e61e84f8da3382d2bedd641d20f6c65bf055
Lara <63805048+larkgz@users.noreply.github.com>
2024-10-17 18:19:19 -0400

    display patrol img on autocomplete

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,14 @@
 import * as vscode from 'vscode';
 import path from 'path';
 
+function createPatrolArtMarkdownString(patrolArtName: string, folder: vscode.WorkspaceFolder|undefined): vscode.MarkdownString {
+  if (folder) {
+    const imgPath = path.join(folder.uri.fsPath, 'resources', 'images', 'patrol_art', `${patrolArtName}.png`);
+    return new vscode.MarkdownString(`![patrol image](${imgPath})`);
+  }
+  return new vscode.MarkdownString('');
+}
+
 export async function activate(context: vscode.ExtensionContext) {
   const PATROL_GLOB = 'resources/images/patrol_art/*.png';
   const folder = vscode.workspace.workspaceFolders?.[0];
@@ -19,6 +27,11 @@ export async function activate(context: vscode.ExtensionContext) {
   const patrol_art_autocomplete_provider = vscode.languages.registerCompletionItemProvider('json', {
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
       return patrolArtNames.map((value) => new vscode.CompletionItem(value), vscode.CompletionItemKind.Value);
+    },
+
+    resolveCompletionItem(item, token) {
+      item.documentation = createPatrolArtMarkdownString(item.label.toString(), folder);
+      return item;
     }
   });
 
